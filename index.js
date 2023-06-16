c493a47b92a7dc21cb276f9a993837560bda2392
Erin Brickman <eelise187@Erins-MBP.attlocal.net>
2023-06-16 12:48:48 -0700

    new way to do it

--- index.js
+++ index.js
@@ -1,85 +1,119 @@
 (function($, undefined) {
     'use strict'
-    let numPlays = 0;
-    let gameEnd = false;
-    let currentPlayer = "X";
-    let currentPlays = {
-        X: [],
-        O: [],
-    };
+    const cells = document.querySelectorAll(".cell");
+    const playerX = "x";
+    const playerO = "o";
+    let currentPlayer = playerX;
 
+// setting data index from cells to note when winning line created //
     const winningLines = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-        [1, 4, 7],
-        [2, 5, 8],
-        [3, 6, 9],
-        [1, 5, 9],
-        [3, 5, 7],
-      ];
-      
+        { combo: [1, 2, 3]},
+        { combo: [4, 5, 6]},
+        { combo: [7, 8, 9]},
+        { combo: [1, 4, 7]},
+        { combo: [2, 5, 8]},
+        { combo: [3, 6, 9]},
+        { combo: [1, 5, 9]},
+        { combo: [3, 5, 7]},
+    ];
+    
+//creating board to start off as empty array//
+    const gameBoard = Array(cells.length);
+    gameBoard.fill(null);
+    
+// calling sections of html to interact //
+    const endGameDiv = document.getElementById("endGame");
+    const endGameText = document.getElementById("endGameText");
+    const playAgain = document.getElementById("start-btn");
+    playAgain.addEventListener("click", startNewGame);
+    
+// something fun I found on a youtube https://www.youtube.com/watch?v=fPew9OI2PnA, so I went and found a sound that works for my page https://soundbible.com/1137-Bubbles.html//
+    const gameWonSound = new Audio("sounds/bubbles.wav");
+    
+// click event listener for when each cell is selected. The hover text was from a youtube and added a little more interactive quality to the board. //
 
-$(document).ready(function () {
-    $(".cell").on("click", function () {
-        if(!gameEnd) {
-                numPlays++;
-                $(this).text(currentPlayer);
-                currentPlays[currentPlayer].push(parseInt($(this).attr("data-index")));
-            if (playerWins()) {
-                gameEnd = true;
-                showGameResult('win');
+    cells.forEach((cell) => cell.addEventListener("click", cellSelect));
+    
+function setHoverText() {
+// hover cannot stay until clicked //
+    cells.forEach((cell) => {
+        cell.classList.remove("x-hover");
+        cell.classList.remove("o-hover");
+      });
+    
+// the hover will be whichever current player symbol is //
+    const hoverClass = `${currentPlayer}-hover`;
+    
+// can only select blank cells to hover over or eventually select. no turning one into the other //
+    cells.forEach((cell) => {
+        if (cell.innerText == "") {
+            cell.classList.add(hoverClass);
             }
-            if(!gameEnd && gameDraw ()) {
-                showGameResult('draw')
-            }  
-        currentPlayer = (currentPlayer === "X") ? "O" : "X";
-      }    
-  });
-});
-
-function playerWins() {
-  if (numPlays < 5) 
-    return;
-    for (let i = 0; i < winningLines.length; i++) {
-        let playerWins = true;
-        for (let j = 0; j < winningLines[i].length; j++) {
-          if ($.inArray(winningLines[i][j], currentPlays[currentPlayer]) < 0) {
-            playerWins = false;
-            break;
-          }
+      });
+}
+        
+setHoverText(); // running hover function at beginning of game/page open //
+    
+// game play //
+function cellSelect(event) {
+    if (endGame.classList.contains("visible")) {
+        return;
         }
-    if(playerWins)
-        return true;
+    // empty cells can be selected //
+    const cell = event.target;
+    const cellNumber = cell.dataset.index;
+    if (cell.innerText != "") {
+        return;
+        }
+    // cells receive currentPlayer symbol and move is recorded in array //
+    if (currentPlayer === playerX) {
+        cell.innerText = playerX;
+        gameBoard[cellNumber - 1] = playerX;
+        currentPlayer = playerO;
+        } else {
+        cell.innerText = playerO;
+        gameBoard[cellNumber - 1] = playerO;
+        currentPlayer = playerX;
+      }  
+    setHoverText(); // running hover function to not hover over filled square //
+    checkWinner(); // calling function to make sure the winning line combos  have not been selected //
+}
+    
+function checkWinner() { //checking cells for winning line combos //
+    for (const winningLine of winningLines) {
+        const { combo} = winningLine;
+        const cellValue1 = gameBoard[combo[0] - 1];
+        const cellValue2 = gameBoard[combo[1] - 1];
+        const cellValue3 = gameBoard[combo[2] - 1];
+        if (
+          cellValue1 != null &&
+          cellValue1 === cellValue2 &&
+          cellValue1 === cellValue3
+        ) {
+          endGameScreen(cellValue1);
+          return; //return used here to make sure it does not call a draw if all squares are used to when. //
+        }
       }
-        return false;
+    const boardFull = gameBoard.every((cell) => cell !== null); // draw situation, full board //
+    if (boardFull) {
+        endGameScreen(null);
+        }
 }
-
-function gameDraw() {
-    return numPlays == 9;
+// function for end game pop up calling winner and resetting with button. this will make the hidden items on html visible for only these conditions of a game win or a full board. also playing the sound idea I found //
+function endGameScreen(endText) {
+    let text = "Game is a draw. Play again!";
+    if (endText != null) {
+        text = `The winner is Player ${endText}!`;
+        }
+    endGameDiv.className = "visible";
+    endGameText.innerText = text;
+    gameWonSound.play();
 }
-
-function showGameResult(type) {
-    gameEnd = true;
-    if (type === 'win') {
-        $('#endMessage-win').text(currentPlayer + " Wins!"); 
-    } else {
-        $('#endMessage-draw').text("Game is a draw. Play again!");
-    }
-
-    $("#endMessage");
-    start-btn.addEventListener( 'click', playAgain);
-}
-
-function playAgain() {
-    numPlays = 0;
-    gameEnd = false;
-    currentPlayer = "X";
-    currentPlays = {
-            X: [],
-            O: [],
-        };
-    $(".cell, #endMessage").text("");
+// resetting game to keep playing. connected to button with addEventListener. next up will be whoever did not win... so if x won, o starts. //
+function startNewGame() {
+    endGameDiv.className = "hidden";
+    gameBoard.fill(null);
+    cells.forEach((cell) => (cell.innerText = ""));
 }
 
 })(jQuery);
